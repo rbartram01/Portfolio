7747f9eac13c12eb26b442b7768e4a6145e17001
rbartram01 <rebecca.bartram@hotmail.co.uk>
2018-05-31 21:05:34 +0100

    Working on Portfolio

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,35 +9,3 @@ $(document).ready(function(){
     pauseOnHover: false
   });
 });
-
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
-});
-
-function submitForm(){
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "resources/php/process.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            }
-        }
-    });
-}
-
-function formSuccess(){
-    $( "#msgSubmit" ).removeClass( "hidden" );
-}
