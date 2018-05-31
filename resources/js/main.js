5acc382d64c1c1da81e4401e7c9a84d3d4d49029
rbartram01 <rebecca.bartram@hotmail.co.uk>
2018-05-31 14:54:10 +0100

    Adding a Contact Form

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,3 +9,30 @@ $(document).ready(function(){
     pauseOnHover: false
   });
 });
+
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "./resources/php/process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
